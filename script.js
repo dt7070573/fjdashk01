eec1e2ea0059b37448a6d9833f452e603c600fc2
gsadsadsa <dimti77@gmail.com>
2025-12-20 16:07:10 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('action-btn');
+const logWindow = document.getElementById('log-window');
+
+btn.addEventListener('click', function(){
+    const newLog = document.createElement('p');
+    newLog.textContent = "캐릭터가 문을 흔들었지만 열리지 않았습니다."}
+
+    logWindow.appendChild(newLog);
+});
