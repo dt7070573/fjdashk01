14f60f211924f993817585ffaf5cc09e8ff1adae
gsadsadsa <dimti77@gmail.com>
2025-12-20 20:36:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const btn = document.getElementById('action-btn');
 const logWindow = document.getElementById('log-window');
 
+const character = {
+    name: "모험가"
+    strength: 5
+    intel:8
+};
+
 btn.addEventListener('click', function(){
     const newLog = document.createElement('p');
 
-    const dice = Math.random();
+    const dice = Math.random() * 10;
 
-    if (dice > 0.5){
-        newLog.textContent = "성공! 캐릭터가 문틈을 발견하고 살짝 열었습니다.";
+    if (character.intel > dice) {
+        newLog.textContent = '[지능 성공] 캐릭터가 암호를 풀었습니다! (주사위: ${dice.toFixed(1)})';
         newLog.style.color = "blue";
     } else {
-        newLog.textContent = "실패... 문이 꽉 잠겨있어 꿈쩍도 하지 않습니다.";
+        newLog.textContent = '[지능 실패] 암호가 너무 복잡합니다. (주사위: ${dice.toFixed(1)})';
         newLog.style.color = "red";
     }
 
     logWindow.appendChild(newLog);
-
     logWindow.scrollTop = logWindow.scrollHeight;
-});
+})
