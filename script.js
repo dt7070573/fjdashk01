8c756959d83afd38285f4dce21ee00cb3e37e38f
gsadsadsa <dimti77@gmail.com>
2025-12-20 16:19:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const logWindow = document.getElementById('log-window');
 
 btn.addEventListener('click', function(){
     const newLog = document.createElement('p');
-    newLog.textContent = "캐릭터가 문을 흔들었지만 열리지 않았습니다."}
+    newLog.textContent = "캐릭터가 문을 흔들었지만 열리지 않았습니다."
 
     logWindow.appendChild(newLog);
 });
