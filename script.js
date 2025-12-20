28500b975a6f87cec41fe970654c6edefb3ea9de
gsadsadsa <dimti77@gmail.com>
2025-12-20 16:41:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const logWindow = document.getElementById('log-window');
 
 btn.addEventListener('click', function(){
     const newLog = document.createElement('p');
-    newLog.textContent = "캐릭터가 문을 흔들었지만 열리지 않았습니다."
+
+    const dice = Math.random();
+
+    if (dice > 0.5){
+        newLog.textContent = "성공! 캐릭터가 문틈을 발견하고 살짝 열었습니다.";
+        newLog.style.color = "blue";
+    } else {
+        newLog.textContent = "실패... 문이 꽉 잠겨있어 꿈쩍도 하지 않습니다.";
+        newLog.style.color = "red";
+    }
 
     logWindow.appendChild(newLog);
+
+    logWindow.scrollTop = logWindow.scrollHeight;
 });
